bf06a97d8997d670f4709843ec5916f8fa58af67
Darshen Nadar <darshennadar@gmail.com>
2026-01-16 22:34:27 +0200

    Update ChessPieces.java

--- ChessPieces.java
+++ ChessPieces.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.chessgame;
 
 /**
@@ -31,3 +27,4 @@ public class ChessPieces {
         return pieceLabel;
     }
 }
+
