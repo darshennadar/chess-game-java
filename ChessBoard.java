6eadb3f8ca0229ecfb1314292ed768626f7bb293
Darshen Nadar <darshennadar@gmail.com>
2026-01-16 22:30:37 +0200

    ChessPieces class added and ChessBoard updated.
    The ChessPieces class allows for the creation of pieces that will be used in the game. The ChessBoard class now has methods to draw and initialize the chess board, as well as a mouse listener to allow for user interaction with the chessgame.

---
+++ ChessBoard.java
@@ -0,0 +1,66 @@
+package com.mycompany.chessgame;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author darsh
+ */
+
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.Color;
+
+
+
+public class ChessBoard extends JPanel {
+    
+    private ChessPieces[][] board = new ChessPieces[8][8];
+
+    public ChessBoard() {
+        
+         setLayout(new GridLayout(8, 8));
+         initialiseBoard();
+         drawBoard();
+    
+    }
+    
+    private void initialiseBoard() {
+                //Placement of Pawns on the board
+        for (int col = 0; col < 8; col++) {
+            board[1][col] = new ChessPieces("Black", "Pawn");
+            board[6][col] = new ChessPieces("White", "Pawn");
+        }
+        //Other Chess Pieces will be added later
+    }
+    
+    private void drawBoard() {
+        
+        removeAll();
+        
+        for (int row = 0; row <8; row++) {
+            
+            for (int col = 0; col < 8; col ++) {
+               
+                 JPanel square = new JPanel();
+                  if ((row + col) % 2 == 0) square.setBackground(Color.WHITE);
+                else square.setBackground(Color.GRAY);
+                  
+                  
+                ChessPieces piece = board[row][col];
+                if (piece != null) {
+                    square.add(piece.getLabel());
+                     }
+
+                add(square);
+            }
+        }
+        revalidate();
+        repaint();
+    }
+}
+
+
---
+++ ChessPieces.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chessgame;
+
+/**
+ *
+ * @author darsh
+ */
+
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class ChessPieces {
+    
+    private String pieceType;
+    private JLabel pieceLabel;
+    private String pieceColour;
+    
+    public ChessPieces (String pieceType, String pieceColour) {
+        
+        this.pieceColour = pieceColour;
+        this.pieceType = pieceType;
+       pieceLabel = new JLabel(pieceType.substring(0,1));
+        if(pieceColour.equals("Black")) pieceLabel.setForeground(java.awt.Color.BLACK);
+        else pieceLabel.setForeground(java.awt.Color.WHITE);
+    }
+        
+ public JLabel getLabel() {
+        return pieceLabel;
+    }
+}
